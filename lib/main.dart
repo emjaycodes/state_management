d940b3a8ed688d1962f42d266e67d8ebb4ffe6bf
emjaycodes <justicenwogu8@gmail.com>
2023-04-10 18:47:37 -0700

    final changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,114 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return ageProvider(
-      age: 0,
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        home: MyHomePage(),
-      ),
+    return const MaterialApp(
+      title: 'Inherited widget',
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+// Counter app with InheritedWidget
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   Widget build(BuildContext context) {
-    int ageNumber = ageProvider.of(context)!.age;
-    // print(ageNumber);
-
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        title: const Text('InheritedWidget Example'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 50,
-            ),
-            Text(
-              ageNumber <= 5
-                  ? 'I am $ageNumber years old'
-                  : 'I am $ageNumber years old',
-              style: const TextStyle(color: Colors.white, fontSize: 40.0),
-            ),
-            const SizedBox(
-              height: 200,
-            ),
-            GestureDetector(
-              onTap: () {
-                ageNumber++;
-                print('age is $ageNumber');
-              },
-              child: Container(
-                height: 50,
-                width: 150,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(50)),
-                child: const Center(
-                    child: Text(
-                  'add age',
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
-                )),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            GestureDetector(
-              onTap: () {
-                ageNumber--;
-                print('age is $ageNumber');
-              },
-              child: Container(
-                height: 50,
-                width: 150,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(50)),
-                child: const Center(
-                    child: Text(
-                  'reduce age',
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
-                )),
-              ),
-            )
-          ],
+    return CounterWidget(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Age counter"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Builder(builder: (context) {
+                final inherited =
+                    context.dependOnInheritedWidgetOfExactType<_InheritedCount>()
+                        as _InheritedCount;
+                return Text(
+                  inherited.state.count <= 1 
+                  ? 'I am ${inherited.state.count} year old' 
+                  : 'I am ${inherited.state.count} years old',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              }),
+              Builder(builder: (context) {
+                final ancestor =
+                    context.findAncestorWidgetOfExactType<_InheritedCount>()
+                        as _InheritedCount;
+                return ElevatedButton(
+                  onPressed: () => ancestor.state.incrementCount(),
+                  child: const Text("Increase Age"),
+                );
+              }),
+               Builder(builder: (context) {
+                final ancestor =
+                    context.findAncestorWidgetOfExactType<_InheritedCount>()
+                        as _InheritedCount;
+                return ElevatedButton(
+                  onPressed: () => ancestor.state.decrementCount(),
+                  child: const Text("Reduce age"),
+                );
+              }),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class ageProvider extends InheritedWidget {
-  final int age;
+class CounterWidget extends StatefulWidget {
+  CounterWidget({Key? key, required this.child}) : super(key: key);
+
   final Widget child;
 
-  ageProvider({
-    super.key,
-    required this.age,
-    required this.child,
-  }) : super(child: child);
+  @override
+  _CounterState createState() => _CounterState();
+}
 
-  static ageProvider? of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<ageProvider>();
+class _CounterState extends State<CounterWidget> {
+  late int count;
+
+  void incrementCount() {
+    setState(() {
+      ++count;
+    });
+  }
+
+   void decrementCount() {
+    setState(() {
+      --count;
+    });
   }
 
   @override
-  bool updateShouldNotify(ageProvider oldWidget) {
-    print('old is ${oldWidget.age}');
-    print('new is ${age}');
-    return oldWidget.age != age;
+  void initState() {
+    super.initState();
+    count = 0;
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return _InheritedCount(
+      state: this,
+      child: widget.child,
+    );
+  }
+}
+
+class _InheritedCount extends InheritedWidget {
+  _InheritedCount({Key? key, required this.state, required Widget child})
+      : super(key: key, child: child);
+
+  final _CounterState state;
+
+  @override
+  bool updateShouldNotify(_InheritedCount old) => true;
 }
